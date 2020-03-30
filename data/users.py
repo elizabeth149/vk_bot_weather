d17c396cdd9c38ac62fd949aabb99d08415a9a2c
eliza_b_e_t_h <gllizaveta@mail.ru>
2020-03-30 12:51:01 +0300

    Настройка БД

---
+++ data/users.py
@@ -0,0 +1,16 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    town = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    idvk = sqlalchemy.Column(sqlalchemy.String,
+                             index=True, unique=True, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
