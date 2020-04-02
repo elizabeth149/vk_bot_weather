b47c615896bcfbd55bd46e2cbf8db2d8ef3e5705
eliza_b_e_t_h <gllizaveta@mail.ru>
2020-04-02 16:02:35 +0300

    Настройка БД2

---
+++ data/answer.py
@@ -0,0 +1,17 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Answer(SqlAlchemyBase):
+    __tablename__ = 'answer'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    ans = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    idvk = sqlalchemy.Column(sqlalchemy.String,
+                             index=True, unique=True, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    town = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
